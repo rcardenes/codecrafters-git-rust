48bda9a22b0dab7fc7e399b15a02ad6930236a3f
Ricardo Cardenes <ricardo.cardenes@noirlab.edu>
2023-11-14 10:49:31 -0300

    Basic starting code

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,32 @@
-#[allow(unused_imports)]
 use std::env;
-#[allow(unused_imports)]
 use std::fs;
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+use anyhow::{bail, Result};
 
-    // Uncomment this block to pass the first stage
-    // let args: Vec<String> = env::args().collect();
-    // if args[1] == "init" {
-    //     fs::create_dir(".git").unwrap();
-    //     fs::create_dir(".git/objects").unwrap();
-    //     fs::create_dir(".git/refs").unwrap();
-    //     fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
-    //     println!("Initialized git directory")
-    // } else {
-    //     println!("unknown command: {}", args[1])
-    // }
+fn initialize_git_directory() -> Result<()> {
+    fs::create_dir(".git")?;
+    fs::create_dir(".git/objects")?;
+    fs::create_dir(".git/refs")?;
+    fs::write(".git/HEAD", "ref: refs/heads/master\n")?;
+
+    Ok(())
+}
+
+fn do_command() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    if args[1] == "init" {
+        initialize_git_directory()?;
+        println!("Initialized git directory")
+    } else {
+        bail!("unknown command: {}", args[1]);
+    }
+
+    Ok(())
+}
+
+fn main() {
+    if let Err(error) = do_command() {
+        eprintln!("{error}")
+    }
 }
